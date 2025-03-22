2dcd195b540d852a0b1e4ebf3cdcb5f70ee7425d
Filip Filipov <168378411+filipovvvv@users.noreply.github.com>
2025-03-23 00:25:31 +0200

    Add files via upload

---
+++ score.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+class Score(Turtle):
+    def __init__(self,position):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.goto(position)
+        self.color("white")
+        self.score = 0
+        self.write(f"Score: {self.score}", font=("Arial", 20, "bold"), align = "center")
+
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score}", font=("Arial", 20, "bold"), align = "center")
+
+
+    def game_over(self):
+        self.clear()
+        self.write(f"GAME OVER", font=("Arial", 20, "bold"), align = "center")
+
+    def increase_score(self):
+        self.score += 1
+        self.update_score()
